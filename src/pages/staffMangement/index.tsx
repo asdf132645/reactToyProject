adfe30cc3e453a83ebb7931ad4ad465af7db8852
asdf1326 <amber1326@naver.com>
2023-12-10 14:52:04 +0900

    직원관리 페이지 추가

---
+++ src/pages/staffMangement/index.tsx
@@ -0,0 +1,17 @@
+import { useRouter } from 'next/router';
+
+import {useState} from 'react';
+
+const StaffMangement = () => {
+    const router = useRouter();
+    
+
+
+    return (
+        <div>
+           
+        </div>
+    );
+};
+
+export default StaffMangement;
