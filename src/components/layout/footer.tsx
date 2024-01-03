28e7b51cb5d31722174d5cd0f904fe72bda722d6
Kang Jiyeong <coin@sugarbricks.co.kr>
2024-01-03 15:25:16 +0900

    공통 사용 달력 추가

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
+
+
+export const Footer = () => {
+    return (
+        <>
+        <footer>
+            <div>footer 영역</div>
+        </footer>
+        </>
+    )
+}
+
